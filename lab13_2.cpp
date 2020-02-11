541b74ab440a4ba6333075dbc2abb25d9eba974c
nayrunner <59276659+nayrunner@users.noreply.github.com>
2020-02-11 18:03:07 +0700

    Update lab13_2.cpp

--- lab13_2.cpp
+++ lab13_2.cpp
@@ -3,11 +3,38 @@ using namespace std;
 
 const int N = 5;
 
-void inputMatrix(double [][N]);
+void inputMatrix(double A[][N]){
+   for(int i=0; i<N; i++){
+   cout<<"Row "<<i+1<<": ";
+        for(int j=0; j<N; j++){ 
+			cin>>A[i][j];
+   }
+}
+}
 
-void findLocalMax(const double [][N], bool [][N]);
+void findLocalMax(const double A[][N], bool B[][N]){
+   for(int i=1;i<N-1;i++){
+       for(int j=1;j<N-1;j++){
+        if(A[i][j]>=A[i-1][j] && A[i][j]>=A[i+1][j] && A[i][j]>=A[i][j-1] && A[i][j]>=A[i][j+1])
+		{B[i][j]=true;
+		}
+		else{B[i][j]=false;
+		}
+	   }
+   }
+    for(int x=0;x<N;x++){B[x][0] = false;}
+	for(int y=0;y<N;y++){B[y][N-1] = false;}
+	for(int z=0;z<N;z++){B[0][z] = false;}
+	for(int xz=0;xz<N;xz++){B[N-1][xz] = false;}
+}
 
-void showMatrix(const bool [][N]);
+
+void showMatrix(const bool B[][N]){
+for(int i=0;i<N;i++){
+       for(int j=0;j<N;j++){cout<<B[i][j]<<" ";}
+	   cout<<"\n";
+}
+}
 
 int main(){
 	double A[N][N]; 
